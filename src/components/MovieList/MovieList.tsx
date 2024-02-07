fa8377e18cfb99bcc5f4811611adbb0d16f5655b
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 18:26:51 +0200

    feat: Add MovieList component

---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MovieCard from '../MovieCard/MovieCard';
+import styles from './MovieList.module.scss';
+
+const MovieList = (): JSX.Element => {
+  return (
+    <div className={styles.containerMovieList}>
+      <div className={styles.movieListCard}>
+        <MovieCard title="testtitle" imageUrl="testurl" />
+        <MovieCard title="testtitle" imageUrl="testurl" />
+        <MovieCard title="testtitle" imageUrl="testurl" />
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
