7112b1bd487669965fc469062ae530d5b148c615
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 18:08:20 +0200

    feat: Add MovieCard component

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieCard = (): JSX.Element => {
+  return (
+    <div>
+      <img src='' alt='Image' />
+      <div >Image title</div>
+    </div>
+  );
+};
+
+export default MovieCard;
