86c5fa3cd3e118bee1522882446592ef9b1b78a9
TitasStr <titas.strasunskas@gmail.com>
2024-02-08 00:37:00 +0200

    fix: fixed constant

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -3,5 +3,5 @@ export const TOKEN = 'token';
 export const REG_ROUTE = '/register';
 export const LOG_ROUTE = '/login';
 export const DASH_ROUTE = '/dashboard';
-export const MOVIE_API_URL = API_URL + '/movies';
+export const MOVIE_API_URL = API_URL + '/movie';
 export const AUTH_API_URL = API_URL + '/auth';
