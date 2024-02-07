2f61b8d4856d2f83eca3c686ca9596da10e580b6
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 22:11:10 +0200

    chore: Add constants file

---
+++ src/constants.tsx
@@ -0,0 +1,7 @@
+export const API_URL = "http://localhost:4000/api";
+export const TOKEN = "token";
+export const REG_ROUTE = "/register";
+export const LOG_ROUTE = "/login";
+export const DASH_ROUTE = "/dashboard";
+export const MOVIE_API_URL = API_URL + "/movies";
+export const AUTH_API_URL = API_URL + "/auth";
