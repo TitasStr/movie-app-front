52bfcd55c1d2f0f4568ebbab36d4b75f6db2e67e
TitasStr <titas.strasunskas@gmail.com>
2024-02-08 00:36:20 +0200

    feat: add component logic

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSearchTerm } from '../../features/movie/movieSlice';
 import styles from './Searchbar.module.scss';
+
 const Searchbar = (): JSX.Element => {
+  const [search, setSearch] = useState<string>('');
+  const dispatch = useDispatch();
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newValue = e.target.value;
+    setSearch(newValue);
+    dispatch(setSearchTerm(newValue));
+  };
+
   return (
     <div className={styles.containerSearchbar}>
       <h2>Search for Movies</h2>
-      <input className={styles.searchBarInput} />
+      <input
+        type="text"
+        placeholder="Type a movie name..."
+        value={search}
+        onChange={handleSearch}
+        className={styles.searchBarInput}
+      />
     </div>
   );
 };
