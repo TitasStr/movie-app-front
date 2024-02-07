3cc9c1462650af08c035e987905ab6f4ecc55836
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 22:15:01 +0200

    chore: removed files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -13,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-reportWebVitals();
