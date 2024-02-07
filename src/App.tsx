372d845164c1275c15b132cd4f4d8e56305530da
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 23:32:41 +0200

    feat: Add AppRouter logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm/LoginForm';
-import Navbar from './components/Navbar/Navbar';
-import Searchbar from './components/Searchbar/Searchbar';
-import MovieList from './components/MovieList/MovieList';
+import AppRouter from './components/AppRouter/AppRouter';
 
 function App(): JSX.Element {
   return (
     <>
-      <Navbar />
-      <Searchbar />
-      <LoginForm />
-      <MovieList />
+      <AppRouter />
     </>
   );
 }
