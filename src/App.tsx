8964bca0ad7dab34258dcfc2a63778f9f4f6e07e
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 18:09:01 +0200

    test: Add components to App.tsx for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import LoginForm from "./components/LoginForm/LoginForm"
-
+import LoginForm from './components/LoginForm/LoginForm';
+import Navbar from './components/Navbar/Navbar';
+import Searchbar from './components/Searchbar/Searchbar';
+import MovieCard from './components/MovieCard/MovieCard';
 function App(): JSX.Element {
   return (
-    <LoginForm />
-  )
+    <>
+      <Navbar />
+      <Searchbar />
+      <LoginForm />
+      <MovieCard />
+    </>
+  );
 }
 
 export default App;
