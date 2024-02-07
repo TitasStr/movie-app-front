08a8d7803306651b689bbc827737acac42232e12
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 17:29:06 +0200

    feat: Add Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar =(): JSX.Element => {
+  return (
+    <nav>
+      <p>MovieNest</p>
+      <ul>
+        <li>Username</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
