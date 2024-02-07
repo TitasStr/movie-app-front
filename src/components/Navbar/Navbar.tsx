04be519d7e685d96db3473f28e0c8ce5812206c6
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 17:56:11 +0200

    feat: Add navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React from "react"
+import styles from "./Navbar.module.scss"
 
 const Navbar =(): JSX.Element => {
   return (
-    <nav>
-      <p>MovieNest</p>
-      <ul>
-        <li>Username</li>
-        <li>Logout</li>
+    <nav className={styles.containerNavbar}>
+      <p className={styles.navTitle}>MovieNest</p>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>Hi, Joe</li>
+        <li className={styles.navItem}>Logout</li>
       </ul>
     </nav>
   );
