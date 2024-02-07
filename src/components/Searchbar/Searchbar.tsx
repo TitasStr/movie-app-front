c44c00c366814bd7168e3db1807922683aed3eb3
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 18:00:57 +0200

    feat: Add SearchBar component

---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Searchbar = (): JSX.Element => {
+  return (
+    <div>
+      <h2>Search for Movies</h2>
+      <input
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
