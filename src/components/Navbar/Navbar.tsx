29ce23121fe7b5591c98981f7762e285dfc0cacb
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 23:50:26 +0200

    feat: add component logic

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { useDispatch, useSelector } from 'react-redux';
+import { type AppDispatch, type RootState } from '../../store';
+import { logout } from '../../features/auth/authSlice';
 import styles from './Navbar.module.scss';
-
+import React from 'react';
 const Navbar = (): JSX.Element => {
+  const { isAuth, username } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogout = (): void => {
+    localStorage.removeItem('token');
+    dispatch(logout());
+  };
+
   return (
     <nav className={styles.containerNavbar}>
       <p className={styles.navTitle}>MovieNest</p>
       <ul className={styles.navList}>
-        <li className={styles.navItem}>Hi, Joe</li>
-        <li className={styles.navItem}>Logout</li>
+        {isAuth && <li className={styles.navItem}>Hi, {username}</li>}
+        {isAuth && (
+          <li className={styles.navItem} onClick={handleLogout}>
+            Logout
+          </li>
+        )}
       </ul>
     </nav>
   );
